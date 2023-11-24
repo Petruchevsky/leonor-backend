a61686a1f180491535fb1a321bb12610d8c05c5a
petruchevsky <m.berdichevsky.a@gmail.com>
2023-11-24 17:51:34 -0300

    ready to connect postgres in railway

--- config/database.js
+++ config/database.js
@@ -6,8 +6,8 @@ module.exports = ({ env }) => {
       client: 'postgres',
       connection: {
         connectionString: env('DATABASE_URL'),
-        ssl: env.bool('DATABASE_SSL', false) ? {
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+        ssl: env.bool('DATABASE_SSL', true) ? {
+          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
           ca: env('DATABASE_SSL_CA', undefined), // Opcional, solo si tienes un certificado CA personalizado
         } : false,
       },
@@ -15,7 +15,7 @@ module.exports = ({ env }) => {
         min: env.int('DATABASE_POOL_MIN', 2),
         max: env.int('DATABASE_POOL_MAX', 10),
       },
-      debug: false, // Cambiar a true si necesitas ver más detalles en los logs
+      debug: true, // Cambiar a true si necesitas ver más detalles en los logs
     },
   };
 };
