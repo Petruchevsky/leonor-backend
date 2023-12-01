4dfb6b2b32a46368addcb21836f335e0962cd76f
petruchevsky <m.berdichevsky.a@gmail.com>
2023-12-01 15:06:30 -0300

    empezando api en español

--- config/database.js
+++ config/database.js
@@ -1,21 +1,36 @@
 const path = require('path');
 
 module.exports = ({ env }) => {
+  const useSSL = env.bool('DATABASE_SSL', false);
+
+  const sslConfig = useSSL ? {
+    rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
+    ca: env('DATABASE_SSL_CA', undefined), // Opcional, para certificado CA personalizado
+  } : false;
+
+  const connection = env('DATABASE_URL')
+    ? {
+        connectionString: env('DATABASE_URL'),
+        ssl: sslConfig,
+      }
+    : {
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: sslConfig,
+      };
+
   return {
     connection: {
       client: 'postgres',
-      connection: {
-        connectionString: env('DATABASE_URL'),
-        ssl: env.bool('DATABASE_SSL', true) ? {
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
-          ca: env('DATABASE_SSL_CA', undefined), // Opcional, solo si tienes un certificado CA personalizado
-        } : false,
-      },
+      connection,
       pool: {
         min: env.int('DATABASE_POOL_MIN', 2),
         max: env.int('DATABASE_POOL_MAX', 10),
       },
-      debug: true, // Cambiar a true si necesitas ver más detalles en los logs
+      debug: env.bool('DEBUG', false),
     },
   };
 };
